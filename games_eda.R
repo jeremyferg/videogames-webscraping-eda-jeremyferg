34cea73fedc24267b20d8fd0fc9c3e565e27cdd7
jeremyferg <jeremy.ferguson573@gmail.com>
2023-12-06 20:27:23 -0600

    working on game_info graphs

--- games_eda.R
+++ games_eda.R
@@ -63,88 +63,94 @@ publishers |>
 
 #game counts by per publisher based on series#
 publishers |> 
-  filter(publishers %in% c('nintendo', 'electronic arts', 'activision blizzard',
-                           'square enix', 'sega', 'konami', 'ubisoft', 'take-two',
-                           'bandai namco', 'capcom')) |> 
   inner_join(game_info, join_by(publisher_id)) |>
   inner_join(game_series, join_by(series_id)) |> 
   distinct(pick(publisher_id, game_id, series_id), .keep_all = TRUE) |> 
-  filter(publishers == 'electronic arts') |>
   mutate(series = factor(series)|> fct_infreq() |> fct_rev(),
-         series = fct_lump(series, n = 10)) |> 
+         series = fct_lump(series, n = 20)) |> 
   summarise(n = n(), .by = series) |> 
   ggplot(aes(series, n)) +
-  geom_bar(stat = 'identity') +
+  geom_bar(fill = 'blue4', stat = 'identity') +
   coord_flip() +
-  geom_text(aes(label = signif(n)), nudge_y = 1)
+  geom_text(aes(label = signif(n)), nudge_y = 80) +
+  labs(y = 'Number of Games',
+       x = 'Series')
 
-## looking at genres (general) ##
-publishers |> 
-  filter(publishers %in% c('nintendo', 'electronic arts', 'activision blizzard',
-                           'square enix', 'sega', 'konami', 'ubisoft', 'take-two',
-                           'bandai namco', 'capcom')) |> 
-  inner_join(game_info, join_by(publisher_id)) |>
-  inner_join(game_genres, join_by(genre_id)) |> 
-  distinct(pick(publisher_id, game_id, genre_id), .keep_all = TRUE) |> 
-  mutate(genres = factor(genres)|> fct_infreq() |> fct_rev(),
-         genres = fct_lump(genres, n = 15)) |> 
-  summarise(n = n(), .by = genres) |> 
-  ggplot(aes(genres, n)) +
-  geom_bar(stat = 'identity') +
-  coord_flip() +
-  geom_text(aes(label = signif(n)), nudge_y = 10)
+get_game_info_distr <- function(some_var = game_series, var_id = series_id, 
+                                var_title = series, nudge = 80, 
+                                top_publisher = FALSE){
 
-## looking at genres per publisher ##
-
-publisher_list <- c('nintendo', 'electronic arts', 'activision blizzard',
-                    'square enix', 'sega', 'konami', 'ubisoft', 'take-two',
-                    'bandai namco', 'capcom')
-
-#test for-loop to make the publisher genres#
-for(som_publisher in publisher_list[1:3]){
+  if(top_publisher == TRUE){
+    
+    bar <-
+    publishers |> 
+      filter(publishers %in% publisher_list) |> 
+      inner_join(game_info, join_by(publisher_id)) |>
+      inner_join({{some_var}}, join_by({{var_id}})) |> 
+      distinct(pick(publisher_id, game_id, {{var_id}}), .keep_all = TRUE) |> 
+      mutate({{var_title}} := factor({{var_title}})|> fct_infreq() |> fct_rev(),
+             {{var_title}} := fct_lump({{var_title}}, n = 15))
+  }else{
+    
+    bar <-
+      publishers |> 
+      inner_join(game_info, join_by(publisher_id)) |>
+      inner_join({{some_var}}, join_by({{var_id}})) |> 
+      distinct(pick(publisher_id, game_id, {{var_id}}), .keep_all = TRUE) |> 
+      mutate({{var_title}} := factor({{var_title}})|> fct_infreq() |> fct_rev(),
+             {{var_title}} := fct_lump({{var_title}}, n = 15))
+    
+  } 
   
-publishers |> 
-  filter(publishers %in% c('nintendo', 'electronic arts', 'activision blizzard',
-                           'square enix', 'sega', 'konami', 'ubisoft', 'take-two',
-                           'bandai namco', 'capcom')) |> 
-  inner_join(game_info, join_by(publisher_id)) |>
-  inner_join(game_genres, join_by(genre_id)) |> 
-  distinct(pick(publisher_id, game_id, genre_id), .keep_all = TRUE) |> 
-  filter(publishers == som_publisher)|> 
-  mutate(genres = factor(genres)|> fct_infreq() |> fct_rev(),
-         genres = fct_lump(genres, n = 7)) |> 
-  summarise(n = n(), .by = genres) |> 
-  ggplot(aes(genres, n)) +
-  geom_bar(stat = 'identity') +
+  bar |> 
+  summarise(n = n(), .by = {{var_title}}) |> 
+  ggplot(aes({{var_title}}, n)) +
+  geom_bar(fill = 'blue4', stat = 'identity') +
   coord_flip() +
-  geom_text(aes(label = signif(n)), nudge_y = 10)
-  
-  ggsave(paste0('data/', som_publisher,'.png'))
+  geom_text(aes(label = signif(n)), nudge_y = nudge)
   
 }
+
+#game_series
+get_game_info_distr(some_var = game_series, var_id = series_id, 
+                    var_title = series, nudge = 80, 
+                    top_publisher = FALSE) 
+  l
+  
+
+get_game_info_distr(some_var = game_series, var_id = series_id, 
+                    var_title = series, nudge = 25, 
+                    top_publisher = TRUE)
+
+#game_genres
+get_game_info_distr(some_var = game_genres, var_id = genre_id, 
+                    var_title = genres, nudge = 50, 
+                    top_publisher = FALSE)
+get_game_info_distr(some_var = game_genres, var_id = genre_id, 
+                    var_title = genres, nudge = 10, 
+                    top_publisher = TRUE)
+
 
 ### boxplot of game modes? ###
 
 publishers |> 
-  filter(publishers %in% c('nintendo', 'electronic arts', 'activision blizzard',
-                           'square enix', 'sega', 'konami', 'ubisoft', 'take-two',
-                           'bandai namco', 'capcom')) |> 
+  filter(publishers %in% publisher_list) |> 
   inner_join(game_info, join_by(publisher_id)) |>
   inner_join(game_modes, join_by(mode_id)) |> 
   distinct(pick(publisher_id, game_id, mode_id), .keep_all = TRUE) |> 
   mutate(modes = factor(modes)|> fct_infreq() |> fct_rev(),
          modes = fct_lump(modes, n = 2),
-         #FIX THIS
          days_since = as.numeric(today() - release),
          year = if_else(year('2017-12-31') < year(release), 
-                        'last_five',
-                        'first_five')) |> 
-  #summarise(n = n(), .by = modes) |> 
+                        'Years 2018 - 2023',
+                        'Years 2013 - 2017')) |> 
   ggplot(aes(year, days_since, fill = modes)) +
-  geom_violin() #+
-  #coord_flip() +
-  geom_text(aes(label = signif(n)), nudge_y = 10)
-  
+  geom_violin() + 
+  labs(x = 'Years',
+       y = 'Days Since Game Released',
+       fill = 'Modes',
+       caption = 'Source: Wikipedia')
+
 ################################################################################
   
 ###################
